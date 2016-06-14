785dae1db6c1eb5a56a78c7dcd92d95a5ddd9678
Maxime Beugnon <noach.design@gmail.com>
2016-06-14 17:00:01 +0200

    suite commit fin de journée

--- projetwebforce/app/routes.php
+++ projetwebforce/app/routes.php
@@ -2,38 +2,49 @@
 
 	$w_routes = array(
 
+// Front
 	['GET', '/', 'Default#home', 'default_home'],
 
+	// Connexion
+  ['GET','/login','Default#login', 'login'],
+  ['POST','/login','Default#loginaction', 'loginaction'],
+
+	// Inscription
+	['GET','/register','Default#register', 'register'],
+	['POST','/register','Default#registeraction', 'registeraction'],
+
+	// Déconnexion
+	['GET','/logout','Default#logout', 'logout'],
+
+	// Ajouter une annonce
+  ['GET','/newannonce','Default#newannonce', 'newannonce'],
+  ['POST','/newannonce','Default#newannonceaction', 'newannonceaction'],
+
+	// Mot de passe oublié
+	['GET','/forget','Default#forget', 'forget'],
+	['POST','/forget','Default#forgetaction', 'forgetaction'],
+
+	// Modifier le mot de passe
+	['GET','/passwordmodif','Default#passwordmodif', 'passwordmodif'],
+	['POST','/passwordmodif','Default#passwordmodifaction', 'passwordmodifaction'],
+
+	// Regénérer le mot de passe
+	['GET','/lienspassword','Default#lienspassword', 'lienspassword'],
+	['POST','/lienspasswordaction','Default#lienspasswordaction', 'lienspasswordaction'],
+
+	// Page annonce unique
+	['GET','/detail','Default#detail', 'detail'],
+
+// Back
+  ['GET','/layoutback','View#layoutback', 'layoutback'],
+
+	// Accueil dashboard
+	['GET','/dashboard','Default#dashboard', 'dashboard'],
+
+	// Gestion des utilisateurs
+  ['GET','/manageuser','Default#manageuser', 'manageuser'],
+  ['GET','/edituser/[:id]','Default#edituser', 'edituser'],
+  ['GET','/deleteuser/[:id]','Default#deleteuser', 'deleteuser'],
 
 
-    ['GET','/layoutback','View#layoutback', 'layoutback'],
-    ['GET','/dashboard','Default#dashboard', 'dashboard'],
-
-    ['GET','/newannonce','Default#newannonce', 'newannonce'],
-    ['POST','/newannonce','Default#newannonceaction', 'newannonceaction'],
-
-
-
-    ['GET','/login','Default#login', 'login'],
-    ['POST','/login','Default#loginaction', 'loginaction'],
-
-    ['GET','/register','Default#register', 'register'],
-    ['POST','/register','Default#registeraction', 'registeraction'],
-
-
-    ['GET','/forget','Default#forget', 'forget'],
-    ['POST','/forget','Default#forgetaction', 'forgetaction'],
-
-    ['GET','/passwordmodif','Default#passwordmodif', 'passwordmodif'],
-    ['POST','/passwordmodif','Default#passwordmodifaction', 'passwordmodifaction'],
-
-
-
-    ['GET','/manageuser','Default#manageuser', 'manageuser'],
-    ['GET','/edituser/[:id]','Default#edituser', 'edituser'],
-    ['GET','/deleteuser/[:id]','Default#deleteuser', 'deleteuser'],
-
-    ['GET','/lienspassword','Default#lienspassword', 'lienspassword'],
-    ['POST','/lienspassword','Default#lienspassword', 'lienspassword'],
-    ['GET','/logout','Default#logout', 'logout'],
-
+	);
